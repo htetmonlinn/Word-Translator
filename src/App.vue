58d1003670c9f8e1a31e1f723c068da09a6e8b38
HtetMonLinn <htetmon4512@gmail.com>
2021-10-04 01:10:22 +0630

    creating dictionary

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="text-center" id="app">
+     <h1>Word Translator</h1>
+     <h5>Power by Vue.js</h5>
+     <TranslateForm @formSubmit="translateText"></TranslateForm>
+     <TranslateOutput v-text="translatedText"></TranslateOutput>
+  </div>
+</template>
+<script>
+import TranslateOutput from './components/TranslateOutput'
+import TranslateForm from './components/TranslateForm'
+import axios from 'axios'
+export default {
+  components: {
+    TranslateOutput, TranslateForm },
+  name: 'app',
+  data() {
+    return {
+      translatedText: '',
+      
+      
+      
+    }
+  },
+  methods:{
+   translateText(text){
+     axios.get(`https://www.dictionaryapi.com/api/v3/references/sd4/json/${text}?key=702e0eac-f291-4e28-8949-be0226b6ce90&lang:ru`)
+     .then(res => {
+       this.translatedText= res.data[0].shortdef[0];
+       
+     })
+     .catch((error) => {
+       console.log(error);
+     });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <div  id="translateForm">
+      <div class="col-md-6 col-md-offset-3">
+      <form class="search-box"  @submit.prevent="formSubmit">
+       <input class="form-control" type="text" v-model="textToTranslate" placeholder="enter a word">
+       <br>
+       <input class="btn btn-secondary" type="submit" value="translate">
+      </form>
+  </div>
+  </div>
+</template>
+<script>
+export default {
+    name: 'translateForm',
+    data() {
+        return {
+         textToTranslate: '', 
+         language:'',
+        }
+    },
+    methods:{
+        formSubmit(e){
+            this.$emit('formSubmit',this.textToTranslate)
+           
+        }
+    }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: 'monserrat', sans-serif;
+}
+#translateForm{
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+.search-box{
+  widows: 100%;
+  margin-bottom: 30px;
+}
+.search-box{
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0,0,0,0.25);
+  background-color: rgba(255,255,255,0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search-box:focus{
+  box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
+ background-color: rgba(255,255,255,0.75);
+ border-radius: 16px 0px 16px 0px;
+}
+
+</style>
